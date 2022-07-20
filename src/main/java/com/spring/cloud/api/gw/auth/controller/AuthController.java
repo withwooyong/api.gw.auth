03b7348683211b1d67a3c9f76796172841e6c46e
Ted-iMac <withwooyong@gmail.com>
2022-07-20 18:36:54 +0900

    Initial commit

---
+++ src/main/java/com/spring/cloud/api/gw/auth/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.spring.cloud.api.gw.auth.controller;
+
+import com.spring.cloud.api.gw.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @GetMapping(path = "/")
+    ResponseEntity<String> getHello() {
+        return ResponseEntity.ok("auth hello");
+    }
+
+    /**
+     * http://localhost:8082/auth/token?userName=token
+     */
+    @GetMapping(path = "/token")
+    ResponseEntity<String> getToken(@RequestParam String userName) {
+        String token = authService.getToken(userName);
+        return ResponseEntity.ok(token);
+    }
+}
---
+++ src/main/java/com/spring/cloud/api/gw/auth/service/AuthService.java
@@ -0,0 +1,16 @@
+package com.spring.cloud.api.gw.auth.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.RandomStringUtils;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class AuthService {
+
+    public String getToken(String userName) {
+        String s = RandomStringUtils.randomAlphabetic(10);
+        log.info("random={}", s);
+        return userName + "_" + s;
+    }
+}
